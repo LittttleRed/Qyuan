4e5e6e7df8f5022407bb83b6ea8bf49dc5bfedbc
LittttleRed <2917781292@qq.com>
2025-11-12 20:47:43 +0800

    first

---
+++ qyuan-read-notes/src/main/java/org/example/qyuanreadnotes/QyuanReadNotesApplication.java
@@ -0,0 +1,13 @@
+package org.example.qyuanreadnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QyuanReadNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QyuanReadNotesApplication.class, args);
+    }
+
+}
