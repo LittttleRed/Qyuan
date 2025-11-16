2ba6ae55950bff0fc89a27a4c26e54bc402a9dd0
LittttleRed <2917781292@qq.com>
2025-11-16 14:41:09 +0800

    first

--- qyuan-manage/src/test/java/org/example/qyuanmanage/QyuanManageApplicationTests.java
+++ qyuan-manage/src/test/java/org/example/qyuanmanage/QyuanManageApplicationTests.java
@@ -1,10 +1,10 @@
-package org.example.qyuanreadnotes;
+package org.example.qyuanmanage;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class QyuanReadNotesApplicationTests {
+class QyuanManageApplicationTests {
 
     @Test
     void contextLoads() {
