ef3b112d3e763d1bf56b648b7bdf1f30af8f4494
LittttleRed <2917781292@qq.com>
2025-11-19 20:55:38 +0800

    first

---
+++ qyuan-order/src/main/java/org/example/qyuanorder/QyuanOrderApplication.java
@@ -0,0 +1,13 @@
+package org.example.qyuanorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QyuanOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QyuanOrderApplication.class, args);
+    }
+
+}
